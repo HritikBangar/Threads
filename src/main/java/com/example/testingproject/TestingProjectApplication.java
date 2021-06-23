9016204d6be68e2f9a4f5dd816606ed8b9ace1b3
HritikBangar <bangarhritik@gmail.com>
2021-06-24 01:49:42 +0530

    initialCommit

---
+++ src/main/java/com/example/testingproject/TestingProjectApplication.java
@@ -0,0 +1,17 @@
+package com.example.testingproject;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+
+//@SpringBootApplication
+public class TestingProjectApplication {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(TestingProjectApplication.class, args);
+
+    }
+
+}
